aa94e06b1276e673330b2ff7e541d286cbf38f77
skrakib <ayfskm@gmail.com>
2025-05-22 01:06:23 +0600

    migrate and update database with my localdob db connection string

--- obj/Debug/net8.0/TodoListMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoListMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+259c92a548675cdb76c2b6b29cac8f1c423cd976")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be7cff9851531255b560c4a7044d39147f16c2af")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
