b495afe1a3fa50c98175477557228f6b61c3a947
skrakib <ayfskm@gmail.com>
2025-05-22 00:37:24 +0600

    Add taskItem.cs model, ignore .vs folder

---
+++ Models/TaskItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoListMVC.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public string Status { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
